ae4284775a6fb7e83cca7b27073bdea873460bc5
GangadharBalija <142907142+GangadharBalija@users.noreply.github.com>
2023-08-25 13:17:08 +0530

    Add files via upload

--- ShoppingAPI/Models/Products.cs
+++ ShoppingAPI/Models/Products.cs
@@ -8,7 +8,7 @@ namespace ShoppingAPI.Models
 
         public int Productid { get; set; }
         public string? ProductName { get; set; }
-        public int ProductCategory { get; set; }
+        public string ProductCategory { get; set; }
         public int ProductPrice { get; set; }
         public int ProductDiscountRate { get; set; }
         public bool ProductIsinStock { get; set; }
@@ -27,17 +27,13 @@ namespace ShoppingAPI.Models
         //};
         static List<Products> ProdList  = new List<Products>()
         {
-            new Products(){ Productid = 1,ProductName="Soap",ProductCategory=1,ProductPrice=10,ProductDiscountRate=2,ProductIsinStock=true },
-            new Products(){Productid = 2,ProductName = "Ball",ProductCategory = 2,ProductPrice = 20,ProductDiscountRate = 4,ProductIsinStock = true},
-            //new Products(){ empNo=103, empName="Pari", empDesignation="Consultant", empDeptNo=10, empIsPermenant = false, empSalary=100},
-            //new Employees(){ empNo=104, empName="Nikita", empDesignation="Consultant", empDeptNo=10, empIsPermenant = true, empSalary=400},
-            //new Employees(){ empNo=105, empName="Anjali", empDesignation="Consultant", empDeptNo=30, empIsPermenant = true, empSalary=600},
-            //new Employees(){ empNo=106, empName="Surbhi", empDesignation="Sales", empDeptNo=30, empIsPermenant = true, empSalary=800},
-            //new Employees(){ empNo=107, empName="Maahi", empDesignation="Consultant", empDeptNo=10, empIsPermenant = false, empSalary=1200},
-            //new Employees(){ empNo=108, empName="Akshay", empDesignation="Consultant", empDeptNo=10, empIsPermenant = true, empSalary=2200},
-            //new Employees(){ empNo=109, empName="Puneet", empDesignation="HR", empDeptNo=20, empIsPermenant = true, empSalary=100},
-            //new Employees(){ empNo=1010, empName="Raj", empDesignation="Consultant", empDeptNo=10, empIsPermenant = true, empSalary=220},
-            //new Employees(){ empNo=1011, empName="Roshni", empDesignation="HR", empDeptNo=20, empIsPermenant = false, empSalary=250}
+          new Products(){ Productid = 1,ProductName="Soap",ProductCategory="A",ProductPrice=10,ProductDiscountRate=2,ProductIsinStock=true },
+          new Products(){Productid = 2,ProductName = "Ball",ProductCategory ="B",ProductPrice = 20,ProductDiscountRate = 4,ProductIsinStock = true},
+          new Products() { Productid = 3,ProductName="Hard",ProductCategory="C",ProductPrice=10,ProductDiscountRate=2,ProductIsinStock=true },
+          new Products() { Productid = 4,ProductName = "Cool",ProductCategory = "D",ProductPrice = 15,ProductDiscountRate = 4,ProductIsinStock = false },
+          new Products() { Productid = 5,ProductName = "Soft",ProductCategory = "E",ProductPrice = 100,ProductDiscountRate = 9,ProductIsinStock = true },
+          new Products() { Productid = 6,ProductName = "CTS",ProductCategory = "F",ProductPrice = 500,ProductDiscountRate = 1,ProductIsinStock = false },
+          new Products() { Productid = 7,ProductName = "GANG",ProductCategory = "G",ProductPrice = 90,ProductDiscountRate = 5,ProductIsinStock = true }
         };
 
        
@@ -51,6 +47,58 @@ namespace ShoppingAPI.Models
             if (Prod != null)
             {
                 return Prod;
+            }
+            throw new Exception("Product Not Found");
+        }
+        public List<Products> GetProductByCategory (string Category )
+        {
+            var Prod  = ProdList.FindAll(p  => p.ProductCategory == Category);
+            if (Prod.Count > 0)
+            {
+                return Prod;
+            }
+            throw new Exception("Sorry Not found this category= " + Category);
+        }
+        public List<Products> GetProductIsinStock (string Yesorno )
+        {
+            if (Yesorno == "yes")
+            {
+                var Isinstock  = ProdList.FindAll(p  => p.ProductIsinStock == true);
+                return Isinstock;
+            }
+            var Instock  = ProdList.FindAll(p  => p.ProductIsinStock == false);
+            return Instock;
+        }
+        public string AddNewProduct(Products newProd )
+        {
+            ProdList.Add(newProd);
+            return "Product Added Successfully";
+        }
+
+        public string UpdateProduct (Products changes)
+        {
+            var Prod  = ProdList.Find(p  => p.Productid == changes.Productid);
+            if (Prod != null)
+            {
+                Prod.ProductName = changes.ProductName;
+                Prod.ProductCategory = changes.ProductCategory;
+                Prod.ProductPrice = changes.ProductPrice;
+                Prod.ProductDiscountRate = changes.ProductDiscountRate;
+                Prod.ProductIsinStock = changes.ProductIsinStock;
+
+                return "Product Details Updated";
+            }
+            throw new Exception("Product Not Found");
+
+        }
+
+        public string DeleteProduct (int Prodid )
+        {
+            var Prod  = ProdList.Find(p  => p.Productid == Prodid);
+            if (Prod  != null)
+            {
+                ProdList .Remove(Prod);
+                return "Product Deleted Successfully";
             }
             throw new Exception("Product Not Found");
         }
